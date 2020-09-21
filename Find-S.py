fc44183410b579fe748358cd7bbd788d8d5fa9ca
paramesha-k <39130142+paramesha-k@users.noreply.github.com>
2020-09-21 15:26:53 +0530

    Add files via upload

---
+++ Find-S.py
@@ -0,0 +1,22 @@
+import pandas as pd
+
+data = pd.read_csv("/content/sample_data/EnjoySport.csv", header=None)
+print(data)
+
+number_of_attributes=6
+hypothesis=['0']*number_of_attributes
+
+print("H-0:",hypothesis)
+
+for index, row in data.iterrows():
+  if row[len(row)-1]== 'Yes':
+    for colIndex in range(len(row)-1):
+      if hypothesis[colIndex]=='0':
+        hypothesis[colIndex]=row[colIndex]
+      elif hypothesis[colIndex] != row[colIndex]:
+        hypothesis[colIndex]='?'
+      else:
+        pass
+    print("H-",str(index+1),":",hypothesis)   
+
+print("S:",hypothesis)    
